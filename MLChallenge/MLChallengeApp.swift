4f29be56e4903389718bda52423a0eaf3a802df3
Layza Carneiro <layza.mrcarneiro@gmail.com>
2025-01-30 10:41:32 -0300

    Adding KNN algorithm

---
+++ MLChallenge/MLChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLChallengeApp.swift
+//  MLChallenge
+//
+//  Created by Layza Maria Rodrigues Carneiro on 30/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MLChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MLChallengeTests/MLChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MLChallengeTests.swift
+//  MLChallengeTests
+//
+//  Created by Layza Maria Rodrigues Carneiro on 30/01/25.
+//
+
+import Testing
+@testable import MLChallenge
+
+struct MLChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
